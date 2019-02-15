f01e6651845639a3b0a6205e22a6b220c4a41927
糖小米 <tangxiaomiemail@gmail.com>
2019-02-15 15:08:50 +0800

    add

---
+++ hello/hello.ts
@@ -0,0 +1,13 @@
+// function sayHello(person: string) {
+//     return 'Hello, ' + person;
+// }
+
+// let user = 'Tom';
+// console.log(sayHello(user));
+
+function sayHello(person: string) {
+    return 'Hello, ' + person;
+}
+
+let user = [0, 1, 2];
+console.log(sayHello(user));
